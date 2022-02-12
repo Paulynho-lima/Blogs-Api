c0925f83984b0c623ce3f44113ea690c7b0784b6
Paulo Lima <paulynho_lima@hotmail.com>
2022-02-12 13:49:21 -0300

    requist 09 resolv

--- controllers/getBlogPostControllers.js
+++ controllers/getBlogPostControllers.js
@@ -3,8 +3,8 @@ const { BlogPost, User, Category } = require('../models');
 const getBlogPostControllers = async (req, res, next) => {
     try {
         const posts = await BlogPost.findAll({
-            include: [{ model: User, as: 'user' },
-            
+            include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } },
+
               { model: Category, as: 'categories', through: { attributes: [] } }],
         });
         return res.status(200).json(posts);
@@ -14,6 +14,25 @@ const getBlogPostControllers = async (req, res, next) => {
     }
 };
 
+const getBlogPostByIdControl = async (req, res, next) => {
+    try {
+        const { id } = req.params; 
+        const blog = await BlogPost.findOne({
+            where: { id },
+            include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } },
+              { model: Category, as: 'categories', through: { attributes: [] } }],
+        });
+
+        if (!blog) return res.status(404).json({ message: 'Post does not exist' });
+
+        return res.status(200).json(blog);
+    } catch (error) {
+        console.log(`POST_ERROR_BAY_ID: ${error.message}`);
+        next(error);
+    }
+};
+
 module.exports = {
     getBlogPostControllers,
+    getBlogPostByIdControl,
 };
--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const loginControllers = require('./controllers/loginControllers');
 const { emailValid, dispNameValid, passwordValid, loginEmailValid,
    loginPasswordValid, nameValidCategory, blogPostValid } = require('./middlewares/validations');
 const { getCategoryController } = require('./controllers/getCategoriesContr');
-const { getBlogPostControllers } = require('./controllers/getBlogPostControllers');
+const { getBlogPostControllers,
+ getBlogPostByIdControl } = require('./controllers/getBlogPostControllers');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -26,6 +27,7 @@ app.get('/categories', errorAuth, getCategoryController);
 app.use('/categories/', errorAuth, nameValidCategory, categoriesControllers);
 
 app.get('/post', errorAuth, getBlogPostControllers);
+app.get('/post/:id', errorAuth, getBlogPostByIdControl);
 app.use('/post/', errorAuth, blogPostValid, blogPostControllers);
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
